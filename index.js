8ec828f9f973aceab022c164d1bcebb0ccc6140a
Ryan Wu <ryan.a.wu.23@dartmouth.edu>
2020-10-25 21:21:09 -0400

    info and tweeks

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 
 function openPage(event, name) {
-  // Declare all variables
+    //set back to top of page
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
+    // hide others
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
-
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
-    // Show the current tab, and add an "active" class to the link that opened the tab
     document.getElementById(name).style.display = "block";
     event.currentTarget.className += " active";
 }
+function iconInfo(id){
+  var iconcontent = document.getElementsByClassName("iconcontent");
+  for (i = 0; i < iconcontent.length; i++) {
+    iconcontent[i].style.display = "none";
+  }
+  var x = document.getElementById(id);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
 document.getElementById("defaultOpen").click();
+var iconcontent = document.getElementsByClassName("iconcontent");
+for (i = 0; i < iconcontent.length; i++) {
+  iconcontent[i].style.display = "none";
+}
